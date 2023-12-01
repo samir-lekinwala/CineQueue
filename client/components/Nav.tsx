9eaff266dcc764cf0e2463610076609ae9e66c4d
Samir Lekinwala <samirlekinwala@gmail.com>
2023-12-01 13:29:59 +1300

    a tag for logo takes back to '/' route temporarily

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -21,7 +21,7 @@ function Nav() {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://flowbite.com/"
+            href="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
