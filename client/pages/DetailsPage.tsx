d5274e8421d8a918691a66ef76126e8bb9dfaa42
Samir Lekinwala <142699675+samir-lekinwala@users.noreply.github.com>
2023-12-01 15:46:26 +1300

    Revert "Fixing page links"

--- client/pages/DetailsPage.tsx
+++ client/pages/DetailsPage.tsx
@@ -1,16 +1,7 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
 
 function DetailsPage() {
-  const { id, type } = useParams()
-
-  return (
-    <div>
-      DetailsPage
-      <p>{id}</p>
-      <p>{type}</p>
-    </div>
-  )
+  return <div>DetailsPage</div>
 }
 
 export default DetailsPage
