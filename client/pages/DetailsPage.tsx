ae15301d64efe1f07599b41828deb5f905ee0d25
Samir Lekinwala <samirlekinwala@gmail.com>
2023-12-01 13:23:42 +1300

    Add temporary params data to be displayed on details page

--- client/pages/DetailsPage.tsx
+++ client/pages/DetailsPage.tsx
@@ -4,7 +4,13 @@ import { useParams } from 'react-router-dom'
 function DetailsPage() {
   const { id, type } = useParams()
 
-  return <div>DetailsPage</div>
+  return (
+    <div>
+      DetailsPage
+      <p>{id}</p>
+      <p>{type}</p>
+    </div>
+  )
 }
 
 export default DetailsPage
