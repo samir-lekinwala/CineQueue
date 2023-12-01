b8adf9773c2232230df6949b83856d388b7d24b2
Logan <140151668+logan-bruce01@users.noreply.github.com>
2023-12-01 13:28:18 +1300

    navbar doesnt push out the header column

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,11 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import { NavGroup, NavButton } from './Styled.tsx'
+import { useState } from 'react'
+import DummyNav from './DummyNav.tsx'
 
 function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
   // TODO: replace placeholder user object with the one from auth0
   const { user, logout, loginWithRedirect } = useAuth0()
+
+  const [toggledNavMenu, setToggledNavMenu] = useState(true)
 
   const handleSignOut = () => {
     logout()
@@ -15,9 +19,13 @@ function Nav() {
     loginWithRedirect()
   }
 
+  function handleNavMenuClick() {
+    setToggledNavMenu((state) => !state)
+  }
+
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 h-15">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="https://flowbite.com/"
@@ -36,9 +44,10 @@ function Nav() {
           <NavGroup>
             <IfAuthenticated>
               <NavButton onClick={handleSignOut}></NavButton>
+
               {/* {user && <p>Signed in as: {user?.nickname}</p>} */}
 
-              <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+              <div className="flex flex-col items-end md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button
                   type="button"
                   className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
@@ -46,6 +55,7 @@ function Nav() {
                   aria-expanded="false"
                   data-dropdown-toggle="user-dropdown"
                   data-dropdown-placement="bottom"
+                  onClick={handleNavMenuClick}
                 >
                   <span className="sr-only">Open user menu</span>
                   <img
@@ -58,61 +68,50 @@ function Nav() {
                   />
                 </button>
                 {/* <!-- Dropdown menu --> */}
-                <div
-                  className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                  id="user-dropdown"
-                >
-                  <div className="px-4 py-3">
-                    <span className="block text-sm text-gray-900 dark:text-white">
-                      Bonnie Green
-                    </span>
-                    <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                      name@flowbite.com
-                    </span>
-                  </div>
-                  <ul className="py-2" aria-labelledby="user-menu-button">
-                    <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                      >
-                        Dashboard
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                      >
-                        Sign out
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <button
-                  data-collapse-toggle="navbar-user"
-                  type="button"
-                  className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                  aria-controls="navbar-user"
-                  aria-expanded="false"
-                >
-                  <span className="sr-only">Open main menu</span>
-                  <svg
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 17 14"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 1h15M1 7h15M1 13h15"
-                    />
-                  </svg>
-                </button>
+                {toggledNavMenu && (
+                  <>
+                    {/* <button
+                      data-collapse-toggle="navbar-user"
+                      type="button"
+                      className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                      aria-controls="navbar-user"
+                      aria-expanded="false"
+                    > */}
+                    <div
+                      className="z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 absolute inline"
+                      id="user-dropdown"
+                    >
+                      <div className="px-4 py-3">
+                        <span className="block text-sm text-gray-900 dark:text-white">
+                          Bonnie Green
+                        </span>
+                        <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                          name@flowbite.com
+                        </span>
+                      </div>
+                      <ul className="py-2" aria-labelledby="user-menu-button">
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                          >
+                            Dashboard
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                          >
+                            Sign out
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+
+                    {/* </button> */}
+                  </>
+                )}
               </div>
             </IfAuthenticated>
             <IfNotAuthenticated>
