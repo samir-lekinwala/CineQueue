2102ed3de34e45c1fb359a37fb6fe54d708dc0a2
Samir Lekinwala <samirlekinwala@gmail.com>
2023-12-04 14:06:09 +1300

    checkout for now

--- client/components/Carousel.tsx
+++ client/components/Carousel.tsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Posters from './Posters'
 
 function Carousel(props: Props) {
   const tmdbPosterLink = `https://image.tmdb.org/t/p/w500/`
 
   return (
-    <div className="carousel-container relative overflow-hidden">
-      <div className="carousel-wrapper flex">
-        {/* <div className="flex justify-center">
+    <>
+      <div className="carousel-container relative overflow-hidden">
+        <div className="carousel-wrapper flex">
+          {/* <div className="flex justify-center">
       <div className="flex flex-wrap gap-4"> */}
-        {props.contentList.map((movie, index) => (
-          <div key={movie.id} className="poster flex-none">
-            <Posters
-              type={`movie`}
-              content={movie}
-              tmdbPosterLink={tmdbPosterLink}
-            />
-          </div>
-        ))}
+          {props.contentList.map((movie, index) => (
+            <div key={movie.id} className="poster flex-none">
+              <Posters
+                type={`movie`}
+                content={movie}
+                tmdbPosterLink={tmdbPosterLink}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      {/* <div className="indicators absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2"></div> */}
+    </>
   )
 }
 
