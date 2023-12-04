9420beca23e91f4178513c76527d1ee3e94b2b0e
Danial Shahali <danial.shahali@outlook.com>
2023-12-04 21:40:49 +1300

    finalaised the Shows RT

--- client/components/TvShowDetails.tsx
+++ client/components/TvShowDetails.tsx
@@ -1,12 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from '@tanstack/react-query'
 import { Details } from '../api/types'
 const { VITE_API_KEY } = import.meta.env
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${VITE_API_KEY}`,
+  },
+}
 
 interface Props {
   details: Details
 }
 
 function TvShowDetails(props: Props) {
+  const { details } = props
   const {
     data: runtime,
     isLoading,
@@ -22,47 +32,34 @@ function TvShowDetails(props: Props) {
     return null
   }
 
-  console.log('props', runtime)
-
   function getRunTime() {
     const episodeRunTime = props.details.episode_run_time
-    const lastEpisodeRunTime = props.details.last_episode_to_air.runtime
-    //turn result into an object that can also out last episode to air runtime.
+    const lastEpisodeRunTime = [props.details.last_episode_to_air.runtime]
+    let finalRuntime
+    if (episodeRunTime.length > 0) {
+      const sum = episodeRunTime.reduce(
+        (acc: any, value: any) => acc + value,
+        0
+      )
+      const averageEpisodeRunTime = sum / episodeRunTime.length
+      finalRuntime = averageEpisodeRunTime
+    } else if (lastEpisodeRunTime.length > 0) {
+      const sum = lastEpisodeRunTime.reduce((acc, value) => acc + value, 0)
+      const averageLastEpisodeRunTime = sum / lastEpisodeRunTime.length
+      finalRuntime = averageLastEpisodeRunTime
+    } else {
+      finalRuntime = 'Not Avilable'
+    }
+    console.log('Runtime:', episodeRunTime)
+    console.log('Runtime1:', lastEpisodeRunTime)
+    console.log('Runtime2:', finalRuntime)
+
     return {
       episodeRunTime: episodeRunTime,
       lastEpisodeRunTime: lastEpisodeRunTime,
+      finalRuntime: finalRuntime,
     }
   }
-
-  const apiURL = 'https://api.themoviedb.org/3/tv'
-  async function getTVShowDetails(id: number) {
-    try {
-      const response = await fetch(`${apiURL}/${id}?api_key=${VITE_API_KEY}`)
-      const data = await response.json()
-
-      // Check if last_episode_to_air exists in the response
-      if (data.last_episode_to_air) {
-        const runtime = data.last_episode_to_air.runtime
-        return runtime
-      } else {
-        console.error('last_episode_to_air not found in the API response')
-        return null
-      }
-    } catch (error) {
-      console.error('An error occurred:', error)
-      throw error
-    }
-  }
-  const { details } = props
-  getTVShowDetails(details.id)
-    .then((runtime) => {
-      if (runtime !== null) {
-        console.log('TV Show Runtime:', runtime)
-      }
-    })
-    .catch((error) => {
-      console.error('An error occurred:', error)
-    })
 
   return (
     <section className="bg-black">
@@ -93,7 +90,7 @@ function TvShowDetails(props: Props) {
             Add to completed
           </button>
           <button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-            Runtime: minutes <br />
+            Runtime: {runtime.finalRuntime} minutes <br />
             Total Number of Episodes: {details.number_of_episodes} <br />
             Total Number of Seasons: {details.number_of_seasons}
           </button>
@@ -121,3 +118,36 @@ export default TvShowDetails
 <p>{details.overview}</p>
 </div> */
 }
+
+// "last_episode_to_air": {
+//   "id": 62161,
+//   "name": "Felina",
+//   "overview": "All bad things must come to an end.",
+//   "vote_average": 9.287,
+//   "vote_count": 202,
+//   "air_date": "2013-09-29",
+//   "episode_number": 16,
+//   "episode_type": "finale",
+//   "production_code": "",
+//   "runtime": 56,
+//   "season_number": 5,
+//   "show_id": 1396,
+//   "still_path": "/pA0YwyhvdDXP3BEGL2grrIhq8aM.jpg"
+// }
+
+// Function to fetch TV show details, including runtime
+// const apiURL = 'https://api.themoviedb.org/3/tv'
+// async function getTVShowDetails(id: number) {
+//   const response = await fetch(`${apiURL}/${id}?api_key=${VITE_API_KEY}`)
+//   const data = await response.json()
+//   const runtime = data.last_episode_to_air.runtime
+//   return runtime
+// }
+
+// getTVShowDetails({details.id})
+//   .then((runtime) => {
+//     console.log('TV Show Runtime:', runtime)
+//   })
+//   .catch((error) => {
+//     console.error('An error occurred:', error)
+//   })
