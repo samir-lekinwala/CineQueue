76400fd1fc33cd92f841b2c132cd482b4fe63019
Samir Lekinwala <samirlekinwala@gmail.com>
2023-12-05 11:06:12 +1300

    Fix tables and create more seeds for watchlist. Also fix recommendation links in carousel.

--- server/db/seeds/2_watchlist.js
+++ server/db/seeds/2_watchlist.js
@@ -1,10 +1,11 @@
-
 export async function seed(knex) {
-
   await knex('watchlist').del()
   await knex('watchlist').insert([
-    {content_id: 1, movie_or_show:"movie", user_id: 3},
-    {content_id: 2, movie_or_show:"show", user_id: 2},
-    {content_id: 6, movie_or_show:"movie", user_id: 1},
-  ]);
+    { content_id: 1, movie_or_show: 'movie', auth_id: 3 },
+    { content_id: 5173, movie_or_show: 'show', auth_id: 2 },
+    { content_id: 6, movie_or_show: 'movie', auth_id: 1 },
+    { content_id: 60059, movie_or_show: 'show', auth_id: 2 },
+    { content_id: 1100, movie_or_show: 'show', auth_id: 2 },
+    { content_id: 872585, movie_or_show: 'movie', auth_id: 2 },
+  ])
 }
