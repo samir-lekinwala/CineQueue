56d48f12d7227be72727001040fdeb959debddb5
Samir Lekinwala <samirlekinwala@gmail.com>
2023-12-05 20:16:09 +1300

    Clicking on add to watchlist adds to watchlist db

--- client/components/MovieDetails.tsx
+++ client/components/MovieDetails.tsx
@@ -1,6 +1,8 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { Details } from '../api/types'
 import { addToWatchlist, getWatchlist } from '../api/dbApi'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useEffect } from 'react'
 
 interface Props {
   details: Details
@@ -12,6 +14,37 @@ function MovieDetails(props: Props) {
   const { user, getAccessTokenSilently } = useAuth0()
   const auth0Id = user?.sub
 
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    // Invalidate relevant queries when type or id changes
+
+    queryClient.invalidateQueries(['watchedOrNot', details])
+    queryClient.invalidateQueries(['watchlistChecker', details])
+    // onWatchlistChecker()
+  }, [queryClient, details])
+
+  const {
+    data: watched, //watchlist single item on list or not
+  } = useQuery({
+    queryKey: ['watchedOrNot'],
+    queryFn: onWatchlistChecker,
+  })
+
+  const {
+    data: watchlist,
+    isLoading,
+    error,
+    isError,
+  } = useQuery({
+    queryKey: ['watchlistChecker'],
+    queryFn: totalWatchlist,
+  })
+  if (isLoading) return <h1>Loading...</h1>
+  if (isError) {
+    console.error(error)
+    return null
+  }
+
   // console.log('user: ', user?.sub)
   const toWatchList = {
     content_id: details.id,
@@ -19,21 +52,32 @@ function MovieDetails(props: Props) {
     auth_id: auth0Id,
   }
 
-  async function checkIfOnWatchlist() {
+  async function totalWatchlist() {
     const token = await getAccessTokenSilently()
     return await getWatchlist(token)
   }
-  console.log(checkIfOnWatchlist())
 
-  // console.log('towatchlist: ', toWatchList)
+  console.log(`Whats on watchlist`, watchlist)
 
   //function to add to watchlist
 
   async function handleWatchListClick() {
     const token = await getAccessTokenSilently()
     await addToWatchlist(toWatchList, token)
+    queryClient.invalidateQueries(['watchlistChecker'])
   }
 
+  function onWatchlistChecker() {
+    const result = watchlist.filter((item) => item.content_id == details.id)
+    console.log('result: ', result)
+    if (result.length > 0) {
+      // console.log("watched is true")
+      return true
+      // console.log("watched is true")
+    } else return false
+  }
+
+  console.log('watched or not', watched)
   return (
     // <div className="grid grid-flow-col auto-cols-max">
     //   <img
@@ -52,7 +96,47 @@ function MovieDetails(props: Props) {
             {details.overview}
           </p>
           <div className="gap-7">
-            <button
+            {!onWatchlistChecker() ? (
+              <button
+                onClick={handleWatchListClick}
+                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              >
+                Add to Watchlist
+                <svg
+                  className="w-5 h-5 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            ) : (
+              <button
+                onClick={handleWatchListClick}
+                className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              >
+                Watchlisted
+                <svg
+                  className="w-5 h-5 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            )}
+
+            {/* <button
               onClick={handleWatchListClick}
               className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
@@ -69,7 +153,8 @@ function MovieDetails(props: Props) {
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </button>
+            </button> */}
+
             <button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
               Add to completed
             </button>
