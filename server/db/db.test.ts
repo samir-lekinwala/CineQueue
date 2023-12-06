4b54e04ef4c0144a82a2af53dd32c2548c041fee
Vitor <142807308+vitor-fernandess@users.noreply.github.com>
2023-12-06 16:38:26 +1300

    tests for new db functions

--- server/db/db.test.ts
+++ server/db/db.test.ts
@@ -1,6 +1,8 @@
 import { it, beforeAll, beforeEach, describe, expect } from 'vitest'
 import db from './connection'
 import {
+  addToCompleted,
+  deleteFromCompletedList,
   deleteFromWatchlist,
   getWatchlist,
   insertIntoWatchlistDb,
@@ -65,6 +67,45 @@ describe('Add to watchlist', () => {
 
     const watchlist = await db('watchlist')
       .where('auth_id', watchlistItem.auth_id)
+      .select('content_id', 'movie_or_show', 'auth_id')
+      .first()
+
+    expect(watchlist).toContain(watchlist)
+  })
+})
+
+describe('getCompletedlist', () => {
+  it('should return content in the completed table', async () => {
+    const user = await getWatchlist('2')
+    expect(user).toHaveLength(4)
+    expect(user[0]).toHaveProperty('movie_or_show')
+    expect(user[0]).toHaveProperty('content_id')
+    expect(user[0]).toHaveProperty('id')
+  })
+})
+describe('deleteFromCompletedList', () => {
+  it('should delete content from the completed table', async () => {
+    const listItem = {
+      content_id: 12,
+      movie_or_show: 'movie',
+      auth_id: '12',
+    }
+    await deleteFromCompletedList(listItem)
+
+    const watchlist = await db('watchlist').select()
+    expect(watchlist).not.toContain(listItem)
+  })
+})
+describe('addToCompleted', () => {
+  it('should add a content to completed table', async () => {
+    const listItem = {
+      content_id: 12,
+      movie_or_show: 'movie',
+      auth_id: '12',
+    }
+    await addToCompleted(listItem)
+    const watchlist = await db('seen')
+      .where('auth_id', listItem.auth_id)
       .select('content_id', 'movie_or_show', 'auth_id')
       .first()
 
