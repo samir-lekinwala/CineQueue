36ad4853ab624d084799e5bbd03532794b46b23b
Samir Lekinwala <samirlekinwala@gmail.com>
2024-02-14 16:02:20 +1300

    test

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: './build', // Change this to your desired directory
-  },
-  base: '/CineQueue',
   plugins: [react()],
+  base: './',
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
